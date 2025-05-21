ee3a2a46a578e556ead918a70924bd89673dad36
Ekene Ezeobi <ezeobiekene7@gmail.com>
2025-05-20 21:26:46 -0400

    started frontend

--- 1st_Project/backend/controllers/product.controller.js
+++ 1st_Project/backend/controllers/product.controller.js
@@ -1,63 +1,72 @@
 import mongoose from "mongoose";
 import { Product } from "../models/product.model.js";
 
-export const getProducts =  async (req, res) => {
-    try{ 
-        const products = await Product.find({})
-        res.status(200).json({success: true, data: products})
-    } catch (error) {
-        console.log('error in fetching products:', error.message)
-        res.status(500).json({ success: false, message: 'Server Error'})
-    }
-}
+export const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log("error in fetching products:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
 
-export const createProduct =  async (req, res) => {
-    const product = req.body
+export const createProduct = async (req, res) => {
+  const product = req.body;
 
-    if(!product.name || !product.price || !product.image) {
-        return res.status(400).json({ success: false, message: 'Please provide all field'})
-    }
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all field" });
+  }
 
-    const newProduct = new Product(product)
+  const newProduct = new Product(product);
 
-    try{
-        await newProduct.save()
-        res.status(201).json({success: true, data: newProduct})
-    }catch (error) {
-        console.error('Error in create product:', error.message)
-        res.status(500).json({
-            success: true,
-            message: 'Server Error'
-        })
-    }
-}
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error("Error in create product:", error.message);
+    res.status(500).json({
+      success: true,
+      message: "Server Error",
+    });
+  }
+};
 
-export const updateProduct =  async (req, res) => {
-    const id = req.params.id
-    const product = req.body
+export const updateProduct = async (req, res) => {
+  const id = req.params.id;
+  const product = req.body;
 
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(400).json({mssg: 'Invalid Id'})
-    }
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ mssg: "Invalid Id" });
+  }
 
-    try{
-        const updatedProduct = await Product.findByIdAndUpdate(id, product, {new: true})
-        res.status(200).json({ success: true, data: updatedProduct})
-    }catch (error) {
-         res.status(500).json({ success: false, message: "Server error"})
-    }
-}
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, {
+      new: true,
+    });
+    res.status(200).json({ success: true, data: updatedProduct });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+};
 
-export const deleteProduct =  async (req, res) => {
-    const id = req.params.id
-      
-    try{
-        await Product.findByIdAndDelete(id)
-        res.status(200).json({ success: true, message: "Product deleted"})
-    }catch (error) {
-        console.log('Error in deleting product:', error.message)
-        res.status(404).json({
-            success: false,
-            message: 'Product Not Found'   })
-    }
-}
+export const deleteProduct = async (req, res) => {
+  const id = req.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ mssg: "Invalid Id" });
+  }
+
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Product deleted" });
+  } catch (error) {
+    console.log("Error in deleting product:", error.message);
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+};
