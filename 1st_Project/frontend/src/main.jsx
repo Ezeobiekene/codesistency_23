5a18872146e5b5a7cafb8eb3da0e6946a1916012
Ekene Ezeobi <ezeobiekene7@gmail.com>
2025-05-26 19:30:09 -0400

    stalling

--- 1st_Project/frontend/src/main.jsx
+++ 1st_Project/frontend/src/main.jsx
@@ -1,21 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { Provider } from "@/components/ui/provider"
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider>
+       <App />
+   </Provider>
   </StrictMode>,
 )
 
-// import React from "react"
-// import ReactDOM from "react-dom/client"
-// import App from "./App"
-
-// ReactDOM.createRoot(document.getElementById("root")!).render(
-//   <React.StrictMode>
-//     <Provider>
-//       <App />
-//     </Provider>
-//   </React.StrictMode>,
